af74af10b1dcc9d91a869b5d8d7a445702d45460
Carlos-SSH <carlos.anezeh@me.com>
2021-11-03 11:42:40 -0600

    Add a new function that reverse strings.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-import { stringLength } from "./string/string";
+import { stringLength } from "./Task 1/string";
+import { reverseString } from "./Task 2/reverse";
 import './style.css';
 
 console.log(stringLength("My String"));
+console.log(reverseString("My String"));
